02b10e4c8bbaae114b4465e5bfb7dd36e46e15ef
bailubai <982239553@qq.com>
2022-06-26 19:31:34 +0800

    without redux

---
+++ src/components/Counter.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 0
+        }
+        this.incrementAsync = this.incrementAsync.bind(this);
+        this.incrementIfOdd = this.incrementIfOdd.bind(this);
+        this.onIncrement = this.onIncrement.bind(this);
+        this.onDecrement = this.onDecrement.bind(this);
+    }
+
+    onIncrement() {
+        this.setState({
+            value: ++this.state.value
+        })
+    }
+
+    onDecrement() {
+        this.setState({
+            value: --this.state.value
+        })
+    }
+
+    incrementIfOdd() {
+        if (this.state.value % 2 !== 0) {
+            this.onIncrement()
+        }
+    }
+
+    incrementAsync() {
+        setTimeout(this.onIncrement, 1000)
+    }
+
+  render() {
+   return (
+      <p>
+        Clicked: {this.state.value} times
+        {' '}
+        <button onClick={this.onIncrement}>
+          +
+        </button>
+        {' '}
+        <button onClick={this.onDecrement}>
+          -
+        </button>
+        {' '}
+        <button onClick={this.incrementIfOdd}>
+          Increment if odd
+        </button>
+        {' '}
+        <button onClick={this.incrementAsync}>
+          Increment async
+        </button>
+      </p>
+    )
+  }
+}
+
+export default Counter
